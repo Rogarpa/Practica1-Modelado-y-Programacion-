16844f3d808843b4b8ab4bf345d9aec9ac0e34db
juan759 <jasso.antonio2001@gmail.com>
2020-10-08 19:46:33 -0500

    git nos dejo trabajar

--- src/Suscriptor.java
+++ src/Suscriptor.java
@@ -1,3 +1,10 @@
 public interface Suscriptor{
 
+    /**
+    *
+    *@param cobrodiariodeservicio el cobro diario del servicio correspondiente
+    */
+    public void cobro(int cobrodiariodeservicio){
+
+    }
 }
